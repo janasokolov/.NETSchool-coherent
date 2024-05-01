e63f61251470d397a3b2100339682cec2c6714d0
janasokolov <sokolov.jana1@gmail.com>
2024-05-01 17:10:37 +0300

    First commit

---
+++ homework2/Planetarium.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetariumMass
+{
+    public class Planetarium
+    {
+        private int[] coordinates = new int[3];
+        private double _mass;
+        public int X
+        {
+            get { return coordinates[0]; }
+            set { coordinates[0] = value; }
+        }
+
+        public int Y
+        {
+            get { return coordinates[1]; }
+            set { coordinates[1] = value; }
+        }
+
+        public int Z
+        {
+            get { return coordinates[2]; }
+            set { coordinates[2] = value; }
+        }
+        public double Mass
+        {
+            get { return _mass; }
+            set { _mass = Math.Max(value,0); }
+        }
+
+        public bool IsZero()
+        {
+            return X == 0 && Y == 0 && Z == 0;
+        }
+
+        public double DistanceTo(Planetarium otherPoint)
+        {
+            int xDiff = X - otherPoint.X;
+            int yDiff = Y - otherPoint.Y;
+            int zDiff = Z - otherPoint.Z;
+
+            return Math.Sqrt(xDiff * xDiff + yDiff * yDiff + zDiff * zDiff);
+        }
+
+        
+    }
+}
